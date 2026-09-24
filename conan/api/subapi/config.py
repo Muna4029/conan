52509903af23171f952871d75f24d2caef282b89
rabeyakhatunmuna <rabeyakhatunmuna@Rabeyas-MacBook-Pro.local>
2026-09-24 18:47:30 -0400

    Muna4029__diff__id_533

--- conan/api/subapi/config.py
+++ conan/api/subapi/config.py
@@ -126,6 +126,12 @@ class ConfigAPI:
         """
         return BUILT_IN_CONFS
 
+    @staticmethod
+    def global_conf_list():
+        """ list all the available built-in configurations (global.conf)
+        """
+        return BUILT_IN_CONFS
+
     def clean(self):
         """ reset the Conan home folder to a clean state, removing all the user
         custom configuration, custom files, and resetting modified files
